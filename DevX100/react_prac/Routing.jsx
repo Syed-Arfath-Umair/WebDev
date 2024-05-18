1d203013adbcaef97e9e7ea562ed3d8181e443ea
Syed Arfath Umair <122617223+Syed-Arfath-Umair@users.noreply.github.com>
2024-05-18 12:44:34 +0530

    Update Routing.jsx

--- DevX100/react_prac/Routing.jsx
+++ DevX100/react_prac/Routing.jsx
@@ -55,3 +55,35 @@ function Appbar(){
 }
 
 export default App;
+
+//Using Lazy loading to send only page the user is on rather than the whole bundle
+
+import {  Navbar } from './pages/Navbar';
+import React, { Suspense } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+const Dashboard = React.lazy(()=>import('./pages/Dashboard'))
+const Landing = React.lazy(()=>import('./pages/Landing'))
+function App(){
+  return(
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Appbar />
+        <Routes>
+          <Route path= '/dashboard'   element={<Suspense fallback="loading..."><Dashboard/></Suspense>} />
+          <Route path='/' element={<Suspense fallback="loading..."><Landing/></Suspense>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+function Appbar(){
+  const navigate = useNavigate();
+  return (<div>
+      <button onClick={()=>navigate('/')} >Landing</button>
+      <button onClick={()=>navigate('/dashboard')} >Dashboard</button>
+  </div>)
+}
+
+export default App;
